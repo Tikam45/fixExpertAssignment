ae523ea55c241484219274b718ff9ddb91556e2e
Tikam45 <tikamgupta05122004@gmail.com>
2025-03-20 14:07:33 +0530

    product pages improvisation

---
+++ components/products/dishWasher.tsx
@@ -0,0 +1,198 @@
+import Head from "next/head";
+
+export default function DishWasher() {
+  return (
+    <>
+      <Head>
+        <title>EasyFix Expert - Dish Washer</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        {/* Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+        {/* 3rd Party CSS */}
+        <link rel="stylesheet" href="https://unpkg.com/kursor/dist/kursor.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+      </Head>
+
+      <main>
+        {/* NAVBAR */}
+        <div className="navbar-wrap p-4">
+          <nav className="nav-main fixed-top border navbar rounded-4 navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="#">
+                <img src="/images/album/easy-fix-logo-bg-removed-2.png" alt="brand logo" />
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#nav-main"
+                aria-controls="nav-main"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon" />
+              </button>
+              <div className="collapse navbar-collapse" id="nav-main">
+                <ul className="navbar-nav ms-auto">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#about">
+                      About
+                    </a>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a
+                      className="nav-link dropdown-toggle"
+                      href="#"
+                      id="navbarDropdown"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Services
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a className="dropdown-item" href="#javascript">
+                        Air Conditioner
+                      </a>
+                      <a className="dropdown-item" href="#html-and-css">
+                        Washing Machine
+                      </a>
+                      <a className="dropdown-item" href="#javascript">
+                        Fridge
+                      </a>
+                      <a className="dropdown-item" href="#javascript">
+                        Dish Washer
+                      </a>
+                      <a className="dropdown-item" href="#javascript">
+                        Water Heater
+                      </a>
+                      <a className="dropdown-item" href="#javascript">
+                        Oven
+                      </a>
+                    </div>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#contact">
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+
+        {/* Since the original dish-washer.html contains only a navbar and a footer, 
+            the main body content is omitted. You can add your page-specific content here if needed. */}
+
+        {/* FOOTER */}
+        <footer className="footer d-flex align-items-center" id="footer">
+          <div className="container">
+            <div className="row g-4 p-2 row-cols-sm-3 row-cols-md-4 ">
+              <div className="col-md-4">
+                <h2 className="mb-4 fw-bold text-white">EasyFix Expert</h2>
+                <p className="footer-brief">
+                  An application developer’s typical responsibilities include coding, designing, application management,
+                  troubleshooting, monitoring updates and possible security threats, and providing end user support.
+                </p>
+              </div>
+              <div className="col-md-4">
+                <h6 className="text-white">Quick Links</h6>
+                <ul>
+                  <li>
+                    <a className="cursorp">Home</a>
+                  </li>
+                  <li>
+                    <a className="cursorp">About</a>
+                  </li>
+                  <li>
+                    <a className="cursorp">Services</a>
+                  </li>
+                  <li>
+                    <a className="cursorp">Contact</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-md-4">
+                <h6 className="text-white">Contact Information</h6>
+                <ul>
+                  <li>
+                    <span className="iconcontainer">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path
+                          fill="inherit"
+                          d="M12 6.5A2.5 2.5 0 0 1 14.5 9a2.5 2.5 0 0 1-2.5 2.5A2.5 2.5 0 0 1 9.5 9A2.5 2.5 0 0 1 12 6.5M12 2a7 7 0 0 1 7 7c0 5.25-7 13-7 13S5 14.25 5 9a7 7 0 0 1 7-7"
+                        />
+                      </svg>
+                    </span>
+                    #1, Subramaniyapuram, <br />1st Street South Extension, <br />State Bank Road, Karaikudi
+                  </li>
+                  <li>
+                    <span className="iconcontainer">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path
+                          fill="inherit"
+                          d="M18.93 20q-2.528 0-5.184-1.266t-4.935-3.555t-3.545-4.935T4 5.07q0-.458.3-.763T5.05 4h2.473q.408 0 .712.257t.411.659L9.142 7.3q.07.42-.025.733t-.333.513L6.59 10.592q.616 1.117 1.361 2.076t1.59 1.817q.87.87 1.874 1.62q1.004.749 2.204 1.414l2.139-2.177q.244-.263.549-.347q.304-.083.674-.033l2.103.43q.408.1.662.411t.254.712v2.435q0 .45-.306.75t-.763.3"
+                        />
+                      </svg>
+                    </span>
+                    <a href="tel:++917550331197">+91 75503 31197</a>
+                    <br />
+                    <a href="tel:+918428431197">+91 84284 31197</a>
+                  </li>
+                  <li>
+                    <span className="iconcontainer">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path
+                          fill="inherit"
+                          d="M4.616 19q-.691 0-1.153-.462T3 17.384V6.616q0-.691.463-1.153T4.615 5h14.77q.69 0 1.152.463T21 6.616v10.769q0 .69-.463 1.153T19.385 19zM12 12.116L4 6.885v10.5q0 .269.173.442t.443.173h14.769q.269 0 .442-.173t.173-.443v-10.5zM12 11l7.692-5H4.308zM4 6.885V6v11.385q0 .269.173.442t.443.173H4z"
+                        />
+                      </svg>
+                    </span>
+                    <a href="mailto:info@easyfixexpert.com">info@easyfixexpert.com</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="row mt-5">
+              <div className="col">
+                <p className="text-center">
+                  This website is made with{" "}
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                      <path
+                        fill="currentColor"
+                        d="m12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53z"
+                      />
+                    </svg>
+                  </span>{" "}
+                  by{" "}
+                  <a href="https://salmank07.github.io/">Pixel Crafted Dreams</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+
+        {/* SCRIPTS */}
+        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+        <script src="https://unpkg.com/kursor"></script>
+      </main>
+    </>
+  );
+}
--- pages/[city]/[brand]/[product].tsx
+++ pages/[city]/[brand]/[product].tsx
@@ -1,8 +1,26 @@
 
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "@/styles/scss/_bootstrap.scss"
+import "@/styles/scss/_bootstrap.scss";
+import "@/styles/scss/_fonts.scss";
+import "@/styles/scss/_variables.scss";
+import "@/styles/scss/main.scss";
+import "@/styles/scss/components/slider.scss"
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import WashingMachinePage from '@/components/products/washingMachine';
+import AirConditionerPage from '@/components/products/ac';
+import WaterHeaterPage from '@/components/products/waterHeater';
+import OvenPage from '@/components/products/oven';
+import FridgePage from '@/components/products/fridge';
+import DishWasher from '@/components/products/dishWasher';
+
 export async function getStaticPaths(){
     const cities = ["Karaikudi", "Madurai", "Chennai", "Coimbatore", "Tirpur"];
     const brands = ["Daikin", "Samsung" , "LG", "Whirlpool", "Godrej"];
-    const products = ["AC", "Fridge", "Washing-Machine"];
+    const products = ["AC", "Fridge", "WashingMachine", "dishWasher", "oven", "waterHeater"];
 
     const paths : {params: {
         city: string
@@ -36,169 +54,35 @@ export async function getStaticProps({params}: {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function DetailPage({params}: any) {
+export default function DetailPage({params}: {
+	params: {
+		city: string,
+		brand: string,
+		product: string,
+	}
+}) {
     const {city, brand, product} = params;
 
     return(
-        <>
-            <div>
-                <section className="Booking-form">
-			<div className="container">
-				<div className="booking-form p-4 w-100 w-md-75 mx-auto shadow bg-white custom-border-radius">
-					<form action="">
-						<div className="row g-4 align-items-end">
-							<div className="col-md-4">
-								<label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-								<input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-							</div>
-							<div className="col-md-4">
-								<label htmlFor="exampleInputPassword1" className="form-label">Phone Number</label>
-								<input className="form-control" id="exampleInputPassword1" />
-							</div>
-							<div className="col-md-4">
-								<button type="submit" className="btn btn-primary rounded-pill w-100 p-2">Book Now</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</section>
-		<section className="intro my-10">
-			<div className="container">
-				<div className="row">
-					<h2 className="fw-bold text-center display-5 section-subtitle">{brand} {product} Service and Repair Centre in
-						<span className="gradient-text">{city}</span>
-					</h2>
-					<p className="text-center text-custom-light">Looking for reliable {brand} air conditioner service in {city}? Our {brand} {product} Service Centre in {city} provides expert solutions for all types of {brand} air conditioners, including split AC, window AC, inverter AC, and central air conditioning units.</p>
-				</div>
-				<div className="row">
-					<div className="col-md-6">
-						<div className="p-3 p-md-5 custom-bg custom-border-radius">
-							<h6 className="text-primary text-uppercase section-title">{brand} </h6>
-							<h2 className="fw-bold display-5 section-subtitle"> {brand} 
-								<span className="gradient-text"> {product} </span>
-								Repair Service center in {city}</h2>
-							<p>Are you facing issues with your air conditioner? Whether it's a minor repair, routine maintenance, or a major breakdown, we are here to provide expert solutions for all your air conditioning needs. Our skilled technicians offer top-notch {product} repair, installation, and servicing for both residential and commercial spaces.</p>
-							<ul className="list-group list-group-flush mt-4">
-								<li className="list-group-item d-flex align-items-center">
-									<span className="">
-										<img src="images/album/icon-park-solid--check-one.svg" width="24" height="24" className="me-4" alt="tick-icon"/>
-									</span>Skilled Technicians</li>
-								<li className="list-group-item d-flex align-items-center">
-									<span className="">
-										<img src="images/album/icon-park-solid--check-one.svg" width="24" height="24" className="me-4" alt="tick-icon"/>
-									</span>Affordable and Transparent</li>
-								<li className="list-group-item d-flex align-items-center">
-									<span className="">
-										<img src="images/album/icon-park-solid--check-one.svg" width="24" height="24" className="me-4" alt="tick-icon"/>
-									</span>Quick Service</li>
-								<li className="list-group-item d-flex align-items-center">
-									<span className="">
-										<img src="images/album/icon-park-solid--check-one.svg" width="24" height="24" className="me-4" alt="tick-icon"/>
-									</span>All Brands Supported</li>
-							</ul>
-						</div>
-					</div>
-					<div className="col-md-6">
-						<img src="images/album/pngwing.com (1).png" width="100%" height="100%" alt="{brand} {product} service centre"/>
-					</div>
-				</div>
-			</div>
-		</section>
-		<section className="why-us-brand my-10">
-			<div className="container">
-				<div className="mb-5">
-					<h6 className="text-primary text-uppercase section-title">Why Choose us?</h6>
-					<h2 className="fw-bold display-5 section-subtitle">Why Choose {brand} {product} Repair<br/>Services at
-						<span className="gradient-text">us</span>?</h2>
-					<p className="text-custom-light w-100 w-md-50">At Easyfix Expert, we specialize in providing expert {brand} {product} repair and maintenance services in {city} and the surrounding areas. We are dedicated to ensuring that your {brand} air conditioning unit runs smoothly, efficiently, and lasts for many years. Here's why you should trust us for all your {brand} AC repair and service needs:</p>
-				</div>
-				<div className="row row-cols-1 row-cols-md-4 g-4">
-					<div className="col">
-						<div className="service-card border">
-							<div className="icon-box">
-								<img src="images/album/token--trust.svg" width="32" height="32" alt="token-trust-icon"/>
-							</div>
-							<div className="services-content">
-								<h5 className="service-title fw-semibold">Certified {brand} {product} Technicians</h5>
-								<p>At Easyfix Expert, our technicians are fully trained and certified to handle all {brand} {product} models. We have the expertise to quickly diagnose and repair issues specific to {brand}'s advanced cooling technology, ensuring your {product} runs at its best.</p>
-							</div>
-						</div>
-					</div>
-					<div className="col">
-						<div className="service-card border">
-							<div className="icon-box">
-								<img src="images/album/la--tools.svg" width="32" height="32" alt="tools-icon"/>
-							</div>
-							<div className="services-content">
-								<div>
-									<h5 className="service-title fw-semibold">Genuine {brand} Parts for Reliable Repairs</h5>
-									<p>We prioritize using genuine {brand} parts for any replacements, ensuring the longevity and efficiency of your air conditioner. Using authentic components helps maintain your warranty and guarantees high-quality repairs.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="col">
-						<div className="service-card border">
-							<div className="icon-box">
-								<img src="images/album/fluent--fast-acceleration-20-regular.svg" width="32" height="32" alt="fast-accleration-icon"/>
-							</div>
-							<div className="services-content">
-								<div>
-									<h5 className="service-title fw-semibold">Fast and Efficient Service</h5>
-									<p>We understand the urgency of a malfunctioning AC, especially during the hot months. Our team provides quick and efficient repairs to restore comfort to your home or office as soon as possible. Whether it's a minor issue or a major repair, we work swiftly without compromising on quality.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="col">
-						<div className="service-card border">
-							<div className="icon-box">
-								<img src="images/album/carbon--review.svg" width="32" height="32" alt="carbon-review-icon"/>
-							</div>
-							<div className="services-content">
-								<div>
-									<h5 className="service-title fw-semibold">Customer Satisfaction Guarantee</h5>
-									<p>We are committed to delivering excellent service. If you're not completely satisfied with our repairs, we'll do everything we can to make it right. Our goal is to ensure your {brand} {product} works flawlessly and you’re completely happy with our service.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-		<section className="service-contact">
-			<div className="text-center mb-5">
-				<div className="w-100 d-flex align-items-center justify-content-center">
-					<h6 className="text-primary text-uppercase section-title">Get in Touch with Us</h6>
-				</div>
-				<h2 className="fw-bold display-5 section-subtitle">Have a Question or Need Assistance?<br/>
-					<span className="gradient-text">Contact Us</span>
-					Today!</h2>
-				<a href="#" className="btn btn-primary rounded-pill px-5" role="button" data-bs-toggle="button">Call Us</a>
-			</div>
-			<div className="booking-form p-4 w-100 w-md-50 mx-auto bg-white rounded-4">
-				<h6 className="mb-4 text-custom fw-semibold text-center">
-					<span className="me-2">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16">
-							<path fill="var(--sk-text-custom-color)" fill-rule="evenodd" d="M11.25 2.5a2.25 2.25 0 0 0-2.154 2.904l.13.43l-.317.318l-6.254 6.253l-.53-.53l.53.53a.664.664 0 0 0 .94.94L9.848 7.09l.318-.318l.43.13a2.25 2.25 0 0 0 2.685-3.124l-1.5 1.501a.75.75 0 1 1-1.061-1.06l1.5-1.5a2.24 2.24 0 0 0-.97-.22ZM7.5 4.75a3.75 3.75 0 1 1 3.114 3.696L10.061 9l.939.94l.47-.47l.53-.53l.53.53l1.875 1.875a2.164 2.164 0 1 1-3.06 3.06L9.47 12.53L8.94 12l.53-.53l.47-.47l-.94-.94l-4.345 4.345l-.53-.53l.53.53a2.164 2.164 0 1 1-3.06-3.06L5.939 7L3.5 4.56l-.617.617l-.507-.761l-1-1.5l-.341-.512l.435-.434l.5-.5l.434-.435l.512.341l1.5 1l.761.507l-.616.617L7 5.94l.554-.554A4 4 0 0 1 7.5 4.75m4.5 6.31l1.345 1.345a.664.664 0 0 1-.94.94L11.061 12z" clip-rule="evenodd"></path>
-						</svg>
-					</span>For Service Booking</h6>
-				<form id="submit-to-google-sheet">
-					<div className="mb-3">
-						<label htmlFor="name" className="form-label text-custom-light">Name</label>
-						<input name="name" className="form-control" id="name" aria-describedby="nameHelp"/>
-					</div>
-					<div className="mb-3">
-						<label htmlFor="phone" className="form-label text-body-tertiary">Phone Number</label>
-						<input type="tel" name="phone" className="form-control" id="phone" pattern="[0-9]{10}" placeholder="Enter 10-digit phone number"/>
-					</div>
-					<button type="submit" className="btn btn-primary rounded-pill mt-3 w-100 p-2">Book Now</button>
-				</form>
-			</div>
-		</section>
-
-            </div>
+        <> 
+			{
+				product === "WashingMachine" && <WashingMachinePage city={city}/>
+			}
+			{
+				product === "AC" && <AirConditionerPage city={city}/>
+			}
+			{
+				product === "WaterHeater" && <WaterHeaterPage/>
+			}
+			{
+				product === "oven" && <OvenPage/>
+			}
+			{
+				product === "Fridge" && <FridgePage/>
+			}
+			{
+				product === "DishWasher" && <DishWasher/>
+			}
         </>
     )
 }
